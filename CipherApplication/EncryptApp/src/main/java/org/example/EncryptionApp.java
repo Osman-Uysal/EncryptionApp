cdab23d2a2dbc9b290da24968ff13e000742ac57
Osman Uysal <93253598+Osman-Uysal@users.noreply.github.com>
2023-11-18 18:47:23 +0000

    Add files via upload

---
+++ CipherApplication/EncryptApp/src/main/java/org/example/EncryptionApp.java
@@ -0,0 +1,94 @@
+package org.example;
+
+import java.util.Scanner;
+
+
+public class EncryptionApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose an option:");
+        System.out.println("1. Encrypt");
+        System.out.println("2. Decrypt");
+        int operationChoice = scanner.nextInt();
+        scanner.nextLine(); 
+
+        switch (operationChoice) {
+            case 1:
+                System.out.println("Enter the text to encrypt:");
+                String textToEncrypt = scanner.nextLine();
+
+                System.out.println("Choose a cipher:");
+                System.out.println("a. Caesar Cipher");
+                System.out.println("b. Vigenère Cipher");
+                System.out.println("c. Rail Fence Cipher");
+                char cipherChoice = scanner.nextLine().charAt(0);
+
+                switch (cipherChoice) {
+                    case 'a':
+                        System.out.println("Enter the shift value for Caesar Cipher:");
+                        int caesarShift = scanner.nextInt();
+                        scanner.nextLine(); // Consume newline
+                        String caesarEncrypted = CaesarCipher.encrypt(textToEncrypt, caesarShift);
+                        System.out.println("Encrypted Result (Caesar Cipher): " + caesarEncrypted);
+                        break;
+                    case 'b':
+                        System.out.println("Enter the key for Vigenère Cipher:");
+                        String vigenereKey = scanner.nextLine();
+                        String vigenereEncrypted = VigenereCipher.encrypt(textToEncrypt, vigenereKey);
+                        System.out.println("Encrypted Result (Vigenère Cipher): " + vigenereEncrypted);
+                        break;
+                    case 'c':
+                        System.out.println("Enter the number of rails for Rail Fence Cipher:");
+                        int railFenceRails = scanner.nextInt();
+                        scanner.nextLine(); // Consume newline
+                        String railFenceEncrypted = RailFenceCipher.encrypt(textToEncrypt, railFenceRails);
+                        System.out.println("Encrypted Result (Rail Fence Cipher): " + railFenceEncrypted);
+                        break;
+                    default:
+                        System.out.println("Invalid cipher choice!");
+                        break;
+                }
+                break;
+            case 2:
+                System.out.println("Enter the text to decrypt:");
+                String textToDecrypt = scanner.nextLine();
+
+                System.out.println("Choose a cipher for decryption:");
+                System.out.println("a. Caesar Cipher");
+                System.out.println("b. Vigenère Cipher");
+                System.out.println("c. Rail Fence Cipher");
+                char decryptCipherChoice = scanner.nextLine().charAt(0);
+
+                switch (decryptCipherChoice) {
+                    case 'a':
+                        System.out.println("Enter the shift value for Caesar Cipher:");
+                        int caesarShift = scanner.nextInt();
+                        scanner.nextLine(); // Consume newline
+                        String caesarDecrypted = CaesarCipher.decrypt(textToDecrypt, caesarShift);
+                        System.out.println("Decrypted Result (Caesar Cipher): " + caesarDecrypted);
+                        break;
+                    case 'b':
+                        System.out.println("Enter the key for Vigenère Cipher:");
+                        String vigenereKey = scanner.nextLine();
+                        String vigenereDecrypted = VigenereCipher.decrypt(textToDecrypt, vigenereKey);
+                        System.out.println("Decrypted Result (Vigenère Cipher): " + vigenereDecrypted);
+                        break;
+                    case 'c':
+                        System.out.println("Enter the number of rails for Rail Fence Cipher:");
+                        int railFenceRails = scanner.nextInt();
+                        scanner.nextLine(); // Consume newline
+                        String railFenceDecrypted = RailFenceCipher.decrypt(textToDecrypt, railFenceRails);
+                        System.out.println("Decrypted Result (Rail Fence Cipher): " + railFenceDecrypted);
+                        break;
+                    default:
+                        System.out.println("Invalid cipher choice!");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                break;
+        }
+    }
+}
---
+++ CipherApplication/EncryptApp/src/main/java/org/example/VigenereCipher.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class VigenereCipher {
+    public static String encrypt(String text, String key) {
+        StringBuilder result = new StringBuilder();
+        int textLen = text.length();
+        int keyLen = key.length();
+
+        for (int i = 0; i < textLen; i++) {
+            char character = text.charAt(i);
+            char keyChar = key.charAt(i % keyLen);
+
+            if (Character.isLetter(character)) {
+                char base = Character.isLowerCase(character) ? 'a' : 'A';
+                character = (char) (((character - base + keyChar - base) % 26) + base);
+            }
+
+            result.append(character);
+        }
+
+        return result.toString();
+    }
+
+    public static String decrypt(String text, String key) {
+        StringBuilder result = new StringBuilder();
+        int textLen = text.length();
+        int keyLen = key.length();
+
+        for (int i = 0; i < textLen; i++) {
+            char character = text.charAt(i);
+            char keyChar = key.charAt(i % keyLen);
+
+            if (Character.isLetter(character)) {
+                char base = Character.isLowerCase(character) ? 'a' : 'A';
+                character = (char) (((character - base - (keyChar - base) + 26) % 26) + base);
+            }
+
+            result.append(character);
+        }
+
+        return result.toString();
+    }
+
+    public static String encryptDecrypt(String text, String key) {
+        return encrypt(text, key);
+    }
+}
+
